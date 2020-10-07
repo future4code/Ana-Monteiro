8b6a7d6e0693a41cfc159a8682188d32ff3d7448
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-06 21:22:41 -0300

    adicionando-desafios-2-e-3

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,3 +1,6 @@
+
+/*Exercício Obrigatório e desafio 1 -> Desafio do alert
+
 function adicionaTarefa() {
     const minhaTarefa = document.getElementById("tarefa")
     const diaEscolhido = document.getElementById("dias-semana").value
@@ -7,13 +10,40 @@ function adicionaTarefa() {
         alert ("Tarefa inválida!")
     }
 
-    minhaLista.innerHTML += `<li>${minhaTarefa.value}</li>`
+    minhaLista.innerHTML += `<li onclick="riscarTarefa1(this)">${minhaTarefa.value}</li>`
     minhaTarefa.value = ""
    
 
     console.log(diaEscolhido)
-        
+}/*
+
+/*Desafios 
+
+1. Incluso no exercício obrigatório
+
+2.
+
+function riscarTarefa1(id) {
+    const minhaTarefa = document.getElementById("tarefa")
+    const diaEscolhido = document.getElementById("dias-semana").value
+    const minhaLista = document.getElementById(diaEscolhido)
+
+    id.style.textDecoration="line-through"
 }
+
+3.
+
+function apagarLista(){
+    const minhaTarefa = document.getElementById("tarefa")
+    const diaEscolhido = document.getElementById("dias-semana").value
+    const minhaLista = document.getElementById(diaEscolhido)
+
+    const apagandoLista = document.getElementsByClassName("nome-da-class")
+    for (let i=0; i < apagandoLista.length; i++){
+        apagandoLista[i].innerHTML = ""
+    }
+    return
+}/*
 
 
 
