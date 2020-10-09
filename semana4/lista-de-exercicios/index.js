3567136d21ee277c329c5a88cde8f45362c14217
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-09 15:33:55 -0300

    adicionando-exercicio-1-interpretacao

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,16 @@
+//Exercício de interpretação 1 //
+
+//function conversorDeMoeda(ValorEmDolar){
+// const cotacao = Number(prompt("Informe o valor da cotação do dólar"));
+// return "R$" + (valorEmDolar * cotacao);
+//}
+// const meuDinheiro = conversorDeMoeda(100);
+//console.log(meuDinheiro)
+
+// Essa função inicialmente te faz uma pergunta "Informe a cotação do Dólar", 
+// no valor que vc responder, que inicialmente é um prompt, ele coloca um Number para
+// transformar do valor que vc colocar, de string em number. Após isso ele pede para vc 
+//retornar o "R$" junto com o valorEmDolar (que inserimos no código, no exemplo é o valor 100),
+// esse número é multiplicado pela cotação em dólar que vc inseriu inicialmente no prompt, transformado em number posteriormente. 
+//Ai embaixo nós criamos a constante meuDinheiro para guardar o valor, e converter o valor que inserimos
+//(100), e embaixo imprimimos no console com o console.log a multiplicação de 100 pelo valor inserido no prompt da cotação do dólar.
