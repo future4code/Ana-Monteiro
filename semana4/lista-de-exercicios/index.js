3bc51caa13758409f6012fca72a79f150e95f5cd
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-11 15:14:13 -0300

    objetos-exercicio-2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -275,3 +275,29 @@ console.log(recebendoMensagem)
 
 //Arrays é como uma matriz de valores, coleção de um ou mais objetos, do mesmo tipo e permite que acessemos o elemento pelo índice dele.
 //Devemos criar arrays quando temos poucos, ou vários objetos, já que cada objeto é um elemento de uma array.
+
+//Exercício 2 - Objetos
+
+//Crie uma função chamada criaRetangulo que recebe como parâmetros dois lados (lado1 e lado2) 
+//e retorna um objeto com 4 informações: largura (lado1), altura (lado2), perímetro (2 * (lado1 + lado2)) e área (lado1 * lado2).
+
+
+
+/*
+let criaRetangulo = (lado1,lado2) =>{
+    const retangulo = {
+        largura: lado1,
+        altura: lado2,
+        perímetro: (2*(lado1+lado2)),
+        área: (lado1*lado2)
+    }
+    return retangulo
+}
+
+
+console.log(criaRetangulo(4,3))
+/*
+
+
+
+
