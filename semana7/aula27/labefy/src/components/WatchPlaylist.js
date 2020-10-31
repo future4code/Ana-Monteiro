2954aa6324c6675fad19bb4e6060a651ce3d0e68
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-10-31 00:13:05 -0300

    aparece-playlists-de-nome-apagar-playlist-visualizar-detalhes-playlist

--- semana7/aula27/labefy/src/components/WatchPlaylist.js
+++ semana7/aula27/labefy/src/components/WatchPlaylist.js
@@ -1,18 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
+import WatchPlaylistTrack from "./WatchPlaylistTrack";
+import App from "../App";
 
 
     class WatchPlaylist extends React.Component {
 
         state = {
             playlists: [],
-            playlistsValue: ""
+            playlistsValue: "",
+            watchMusic: true,
+            playlistsTrack: [],
         }
         
         componentDidMount = () => {
             this.catchPlaylists();
         }
+
     
         catchPlaylists = () => {
             axios.get(
@@ -20,7 +25,7 @@ import axios from "axios";
                 {
                     headers: {
                         Authorization: "anapatricia-monteiro-dumont"
-                    }
+                    },
                 }
             )
             .then((response) => {
@@ -31,6 +36,7 @@ import axios from "axios";
                 console.log(err.message);
             });
         };
+
 
         deletePlaylist = (id) => {
             axios.delete(
@@ -48,21 +54,64 @@ import axios from "axios";
                 console.log("Não foi possível deletar!")
             })
         }
+
+        catchPlaylistsTrack = (id) => {
+            axios.get(
+                `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`,{
+                        headers: {
+                            Authorization: "anapatricia-monteiro-dumont"
+                        },
+                    }
+                )
+                .then((response) => {
+                    this.setState({playlists: response.data.result.tracks})
+
+                })
+                .catch((error)=>{
+                    console.log("Não foi possível deletar!")
+                })
+
+                {this.state.playlists.map(playlist=> {
+                    if(id === playlist.id){
+                        console.log(playlist.name)
+                    }
+                })}
+
+            }
+
+           
+    
+        wannaWatchMusic = () =>{
+            this.setState({watchMusic: !this.state.watchMusic})
+            }
     
       render(){
-        const renderedPlaylists = this.state.playlists.map((playlist) => {
+
+        const renderedPlaylistsTracks = this.state.playlists.map((playlist)=>{
             return (<p key={playlist.id}>
+                <button onClick={() => {this.catchPlaylistsTrack(playlist.id)}}>
+                    +
+                </button>
+            </p>
+            );
+        });
+
+
+        const renderedPlaylists = this.state.playlists.map((playlist) => {
+                return (<p key={playlist.id}>
                     {playlist.name}
                     <button  onClick={() => {this.deletePlaylist(playlist.id)}}>
                         X
                     </button>
-                    </p>
-            );
+                    </p>   
+              );   
         });
-    
+
       return (
         <div>
-            {renderedPlaylists}
+           
+           <div>{renderedPlaylists}</div>
+            <div>{renderedPlaylistsTracks}</div>
         </div>
       );
     }
---
+++ semana7/aula27/labefy/src/components/WatchPlaylistTrack.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+
+class WatchPlaylistTrack extends React.Component {
+
+   
+
+  render(){
+    
+  return (
+    <div>
+   
+    </div>
+  );
+}
+}
+
+export default WatchPlaylistTrack;
