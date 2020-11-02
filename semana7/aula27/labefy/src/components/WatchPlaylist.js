4d4be0a9a28edfbd783b1dfe3ca7ef62dc099da2
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-11-02 20:27:15 -0300

    tudo-funciona-menos-adicionar-tracks-a-playlist

--- semana7/aula27/labefy/src/components/WatchPlaylist.js
+++ semana7/aula27/labefy/src/components/WatchPlaylist.js
@@ -1,112 +1,107 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
-import WatchPlaylistTrack from "./WatchPlaylistTrack";
+import AddPlaylistTrack from "./AddPlaylistTrack";
 import App from "../App";
+import Playlists from "./Playlists";
+import api from "../services/api";
 
 const MainDiv = styled.div`
-font-size: 20px;
-`
+  font-size: 20px;
+`;
 
-    class WatchPlaylist extends React.Component {
+class WatchPlaylist extends React.Component {
+  state = {
+    playlists: [],
+    playlistsValue: "",
+    watchMusic: true,
+    playlistsTrack: [],
+    createTrack: true,
+    selPlaylistId: "",
+    playlistDetail: [],
+  };
 
-        state = {
-            playlists: [],
-            playlistsValue: "",
-            watchMusic: true,
-            playlistsTrack: [],
-            playlistsValueTrackName: "",
-            playlistsValueTrackArtist: "",
-            playlistsValueTrackUrl: "",
-            createTrack: true,
-            selPlaylistId: ""
-        }
+  componentDidMount = () => {
+    this.catchPlaylists();
+  };
 
-        componentDidMount = () => {
-            this.catchPlaylists();
-        }
+  componentDidUpdate = () => {
+    this.catchPlaylistsTrack();
+  };
 
-        componentDidUpdate = ()=>{
-            this.catchPlaylistsTrack();
-        }
+  setPlaylistDetails = (tracks) => {
+    this.setState({ playlistDetail: tracks });
+  };
 
-        
-        catchPlaylists = () => {
-            axios.get(
-                    }
-                    this.setState({playlists: response.data.result.tracks})
-                    console.log("peguei o id da musica")
-                    this.setState({playlistsTrack: response.data.result.tracks})
+  catchPlaylists = () => {
+    api
+      .get("/labefy/playlists")
+      .then((response) => {
+        this.setState({ playlists: response.data.result.list });
+        console.log("TA INU O CACTH");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-                })
-                .catch((error)=>{
-                    console.log("Não foi possível deletar!")
-                })
-            }
+  deletePlaylist = (id) => {
+    api
+      .delete(`/labefy/playlists/${id}`)
+      .then(() => {
+        alert("Playlist deletada com sucesso!");
+        this.catchPlaylists();
+      })
+      .catch((error) => {
+        console.log("Não foi possível deletar!");
+      });
+  };
 
+  catchPlaylistsTrack = (id) => {
+    api
+      .get(`/labefy/playlists/${id}/tracks`)
+      .then((response) => {
+        this.setState({ playlists: response.data.result.tracks });
+      })
+      .catch((error) => {
+        console.log("Não foi possível deletar!");
+      });
+  };
 
-
-        deletePlaylist = (id) => {
-            axios.delete(
-                `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`,{
-                    headers: {
-                        Authorization: "anapatricia-monteiro-dumont"
-                    },
-                }
-              )
-            .then(() => {
-                alert("Playlist deletada com sucesso!");
-                this.catchPlaylists();
-            })
-            .catch((error)=>{
-                console.log("Não foi possível deletar!")
-            })
-        }
-
-        catchPlaylistsTrack = (id) => {
-            axios.get(
-                `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`,{
-                        headers: {
-                            Authorization: "anapatricia-monteiro-dumont"
-                        },
-                    }
-                )
-                .then((response) => {
-                    this.setState({playlists: response.data.result.tracks})
-
-                })
-                .catch((error)=>{
-                    console.log("Não foi possível deletar!")
-                })
-            }
-
-        wannaWatchMusic = () =>{
-            this.setState({watchMusic: !this.state.watchMusic})
-            }
-        
-    
-      render(){
-
+  render() {
+    const renderizaButton = this.state.playlists.map((playlist) => {
       return (
-          this.state.playlists.map((playlist)=> {
-              return (<MainDiv key={playlist.id}>
-                <button onClick={() => {this.catchPlaylistsTrack(playlist.id)}}>
-                    +
-                </button>
-                <div>{playlist.name}</div>
-                <div>{playlist.artist}
-                <audio src={playlist.url} value={playlist.url} controls>{playlist.url}</audio>
-                </div>
-                <button  onClick={() => {this.deletePlaylist(playlist.id)}}>
-                        X
-                </button>
-              
-            </MainDiv>
-            );
-            
-          })
+        <p key={playlist.id}>
+          <button
+            onClick={() => {
+              this.deletePlaylist(playlist.id);
+            }}
+          >
+            Excluir essa Playlist acima
+          </button>
+          <button
+            onClick={() => {
+              this.catchPlaylistsTrack(playlist.id);
+            }}
+          >
+            Ver Músicas da Playlist abaixo
+          </button>
+        </p>
       );
-    }
-    }
+    });
+
+    return (
+      <MainDiv>
+        <Playlists
+          playlists={this.state.playlists}
+          setPlaylistDetails={setPlaylistDetails}
+        />
+
+        {renderizaButton}
+        {playlistEdition}
+      </MainDiv>
+    );
+  }
+}
 
 export default WatchPlaylist;
