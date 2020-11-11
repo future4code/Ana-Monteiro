4dc4145f3585568bd667f380aebcfee9e1928374
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-11-11 20:13:59 -0300

    primeira-etapa-trabalho

---
+++ semana9/astrotinder/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import Profile from "./components/Profile";
+import ListaMaches from "./components/ListaMaches";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div>
+      <Profile></Profile>
+      <ListaMaches></ListaMaches>
+    </div>
+  );
+}
+
+export default App;
