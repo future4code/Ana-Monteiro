70e6a04c011323753a87b8f482c5834b2388cdf3
Ana Patrícia Monteiro <Ana-Monteiro@example.com>
2020-11-17 22:32:16 -0300

    tentando-corrigir-problema-no-surge

--- semana10/labex/src/styles.js
+++ semana10/labex/src/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import montain from "./pages/img/main.jpg";
 
 export const MainDiv = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap");
   font-family: "Dancing Script", cursive;
+  background-image: url(${montain});
 `;
