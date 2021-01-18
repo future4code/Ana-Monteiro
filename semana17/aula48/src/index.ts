0b0a8dbf8588e7698731b80ef728c9f6db015383
Ana-Monteiro <anapatricia.c.monteiro@gmail.com>
2021-01-18 18:58:54 -0300

    Exercicio 1

---
+++ semana17/aula48/src/index.ts
@@ -0,0 +1,45 @@
+import express, {Request, Response} from "express"
+import cors from "cors"
+import knex from "knex" // conexão com o banco de dados mysql
+import dotenv from "dotenv" // conexão com o banco de dados que está no .env, proteger os seus dados
+import {AddressInfo} from "net"
+import { getAllNames, getAllTypes, getAllUsers } from "./endpoint/Endpoint"
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+export const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+})
+
+app.get("/user/all", async(req: Request, res: Response) =>{
+    getAllUsers(req, res)
+})
+
+app.get("/user", async(req: Request, res: Response) =>{
+    const name: string = req.query.name as string
+    getAllNames(req, res, name)
+})
+
+app.get("/user/:type", async (req: Request, res: Response) => {
+    const type: string = req.params.type
+    getAllTypes(req, res, type)
+})
+
+const server = app.listen(process.env.PORT || 3003, ()=>{
+    if(server){
+        const address = server.address() as AddressInfo
+        console.log(`Server is running in http://localhost:${address.port}`)
+    }else{
+        console.error(`Failure upon starting server.`)
+    }
+})
---
+++ semana17/aula48/src/query/query.ts
@@ -0,0 +1,31 @@
+import {connection} from "../index"
+
+export const selectAllUsers = async():Promise<any> => {
+    const result = await connection.raw(`
+       SELECT id, name, email, type
+       FROM aula48_exercicio;
+    `)
+ 
+    return result[0]
+ }
+
+ export const selectAllNames = async(name: string):Promise<any> => {
+   const result = await connection.raw(`
+      SELECT id, name, email, type
+      FROM aula48_exercicio
+      WHERE name LIKE "%${name}%";
+   `)
+
+   return result[0]
+}
+
+export const selectAllTypes = async(type: string):Promise<any> => {
+   const result = await connection.raw(`
+      SELECT id, name, email, type
+      FROM aula48_exercicio
+     WHERE type LIKE '%${type}%';
+   `)
+
+   return result[0]
+}
+
