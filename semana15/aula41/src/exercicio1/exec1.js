46b14210bd375de88cc1577614d7cfed49117cae
Ana-Monteiro <anapatricia.c.monteiro@gmail.com>
2021-01-24 20:32:05 -0300

    Exercicio 1 e 2 finalizados aula 41

--- semana15/aula41/src/exercicio1/exec1.js
+++ semana15/aula41/src/exercicio1/exec1.js
@@ -1,30 +1,18 @@
 //1
 //Hoje conhecemos uma "nova linguagem", que é muito parecida com o Javascript, mas que tem alguns adicionais muito importantes para nós. Abaixo temos algumas perguntas e solicitações de escrita de código. Você pode responder as perguntas comentando no arquivo TS.
 //O Typescript é uma linguagem um pouco mais criteriosa que o Javascript, então vamos conhecer um pouco desses critérios.
-
 //a) Crie uma variável **minhaString** do tipo `string` e atribua um valor a ela. Tente atribuir um número a esta variável. O que acontece?
 //O Typescript é uma linguagem um pouco mais criteriosa que o Javascript, então vamos conhecer um pouco desses critérios.
-
 //const minhaString: string = 5
 // Erro: número não é do tipo string
-
 //b) Crie uma variável meuNumero do tipo number e atribua um valor numérico. Como podemos fazer para que essa variável também aceite strings?
 // Colocar union type 
-
 // let meuNumero: number | string  = "olar";
-
-
 //c) Agora crie um novo objeto. Este objeto é uma pessoa, e deve possuir três propriedades:
-
 //`nome`, que é uma string;
-
 //`idade`, que é um número;
-
 //`corFavorita`, que é uma string.
-
 //Como você faria para garantir que o objeto só tenha as propriedades descritas acima? Faça a tipagem do objeto para que ele só aceite os valores acima.
-
-
 /*
 
 type dadosPessoas = {
@@ -42,10 +30,7 @@ const pessoa: dadosPessoas = {
 console.log(pessoa)
 
 */
-
-
 // d)Crie mais três objetos, que também precisam ter apenas os campos definidos acima. Crie um tipo Pessoa para garantir que todos os objetos tenham os mesmos campos.
-
 /*
 
 type dadosPessoas = {
@@ -77,41 +62,24 @@ console.log(pessoa2)
 console.log(pessoa3)
 
 */
-
 // e) Modifique o tipo de objeto para que possamos apenas escolher entre as cores do arco-íris. Utilize um enum para isso.
-
-/*
-
-enum Color {
-    YELLOW = "Yellow",
-    BLUE = "Blue",
-    PINK = "Pink",
-    BLACK = "Black",
-    PURPLE = "Purple"
-}
-
-
-type dadosPessoas = {
-    name: string,
-    age: number,
-    favoriteColor: Color
-}
-
-const pessoa: dadosPessoas = {
+var Color;
+(function (Color) {
+    Color["YELLOW"] = "Yellow";
+    Color["BLUE"] = "Blue";
+    Color["PINK"] = "Pink";
+    Color["BLACK"] = "Black";
+    Color["PURPLE"] = "Purple";
+})(Color || (Color = {}));
+var pessoa = {
     name: "LALA",
     age: 15,
     favoriteColor: Color.BLACK
-}
-
-const pessoa2: dadosPessoas = {
+};
+var pessoa2 = {
     name: "RONALDO",
     age: 25,
     favoriteColor: Color.PINK
-}
-
-console.log(pessoa)
-console.log(pessoa2)
-
-*/
-
-
+};
+console.log(pessoa);
+console.log(pessoa2);
--- semana15/aula41/src/exercicio2/exec2.ts
+++ semana15/aula41/src/exercicio2/exec2.ts
@@ -3,25 +3,62 @@
 /*
 
 Observe a função a seguir, escrita em JavaScript:
-function obterEstatisticas(numeros) {
 
-    const numerosOrdenados = numeros.sort(
+*/
+
+// a. Quais são as entradas e saídas dessa função? Copie a função para um arquivo .ts e faça a tipagem desses parâmetros
+// Entrada vai ser um array de números, e a saída vai ser objeto com o maior valor, menor e a média 
+
+
+
+function obterEstatisticas(numeros: number[]) {
+
+    const numerosOrdenados = numeros.sort(  //Colocando os números do array em ordem, do menor para o maior
         (a, b) => a - b
     )
 
     let soma = 0
 
-    for (let num of numeros) {
+    for (let num of numeros) {             //Aqui vou somando a soma 0, o novo valor do array de números
         soma += num
     }
 
     const estatisticas = {
-        maior: numerosOrdenados[numeros.length - 1],
-        menor: numerosOrdenados[0],
-        media: soma / numeros.length
+        maior: numerosOrdenados[numeros.length - 1], // Pego o tamanho do array, e tiro o último valor que vai ser o maior
+        menor: numerosOrdenados[0],                  // Pego o menor valor que está na posição 0
+        media: soma / numeros.length                // Faço a média
     }
 
     return estatisticas
 }
 
+
+/*
+
+b. Que outras variáveis compõem essa função? Explicite a tipagem de todas elas 
+ 
+// As outras variáveis que compõem são: "numerosOrdenados" (tipagem: array de números ), com variável fixa const e "soma" (tipagem: número) variável let, que permite mudança. E "let num" (tipagem: número) variável
+criada dentro do for, para varrer os valores do array. E "estatisticas" variável const fixa (tipagem: objeto que contem maior, menor, e média todos são do tipo number)
+
+*/ 
+
+//c. Crie um *type* para representar uma **amostra** de dados, isto é, um objeto com as chaves **numeros** e **obterEstatisticas**. Abaixo, temos um exemplo de objeto desse tipo, declarado em JavaScript:
+
+/*
+
+type dados = {
+    numeros: number[],
+    obterEstatisticas: (numeros: number[]) => void
+}
+
+const amostraDeIdades: dados = {
+
+		numeros: [21, 18, 65, 44, 15, 18],
+
+		obterEstatisticas: (numeros) => {console.log(numeros)}
+}
+
+amostraDeIdades.obterEstatisticas(amostraDeIdades.numeros)
+
 */
+
