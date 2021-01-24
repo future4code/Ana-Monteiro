88310e3c5bc6af4d4471afbeb2badb3771d13722
Ana-Monteiro <anapatricia.c.monteiro@gmail.com>
2021-01-24 00:30:08 -0300

    Exercicio 1 feito e 2 em execução

--- semana15/aula41/src/exercicio1/exec1.ts
+++ semana15/aula41/src/exercicio1/exec1.ts
@@ -10,5 +10,64 @@
 //b) Crie uma variável meuNumero do tipo number e atribua um valor numérico. Como podemos fazer para que essa variável também aceite strings?
 // Colocar union type 
 
-let meuNumero: number | string  = "olar";
+// let meuNumero: number | string  = "olar";
 
+//c) Agora crie um novo objeto. Este objeto é uma pessoa, e deve possuir três propriedades:
+
+//`nome`, que é uma string;
+
+//`idade`, que é um número;
+
+//`corFavorita`, que é uma string.
+
+//Como você faria para garantir que o objeto só tenha as propriedades descritas acima? Faça a tipagem do objeto para que ele só aceite os valores acima.
+
+/*
+
+type dadosPessoas = {
+    name: string,
+    age: number,
+    favoriteColor: string
+}
+
+*/
+
+// d)Crie mais três objetos, que também precisam ter apenas os campos definidos acima. Crie um tipo Pessoa para garantir que todos os objetos tenham os mesmos campos.
+
+/*
+
+const pessoa: dadosPessoas = {
+    name: "Rihana",
+    age: 52,
+    favoriteColor: "blue"
+}
+
+const pessoa2: dadosPessoas = {
+    name: "Zé",
+    age: 24,
+    favoriteColor: "yellow"
+}
+
+const pessoa3: dadosPessoas = {
+    name: "Ronaldinho",
+    age: 50,
+    favoriteColor: "black"
+}
+
+*/
+
+// e) Modifique o tipo de objeto para que possamos apenas escolher entre as cores do arco-íris. Utilize um enum para isso.
+
+/*
+
+enum Color {
+    YELLOW = "Yellow",
+    BLUE = "Blue",
+    PINK = "Pink",
+    BLACK = "Black",
+    PURPLE = "Purple"
+}
+
+*/
+
+// DÚVIDA: NÃO ESTOU CONSEGUINDO EXECUTAR OS COMANDOS ACIMA, TENTEI APAGAR OS NODE_MODULES E ELE NÃO FOI EXECUTADO
---
+++ semana15/aula41/src/exercicio2/exec2.ts
@@ -0,0 +1,27 @@
+// Exercicio 2
+
+/*
+
+Observe a função a seguir, escrita em JavaScript:
+function obterEstatisticas(numeros) {
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+*/
