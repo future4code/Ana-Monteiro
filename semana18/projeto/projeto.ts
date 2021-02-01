d7f13ea5605c8a156405b19f1d76349d99ed2bc3
Ana-Monteiro <anapatricia.c.monteiro@gmail.com>
2021-02-01 18:38:44 -0300

    Configurando o workstation

--- semana18/projeto/projeto.ts
+++
@@ -1,9 +0,0 @@
-/*
-
-Esse produto nada mais é do que uma rede social, na qual os usuários podem dividir informações relevantes sobre comidas e receitas que tenham experimentado. Ela possui todas as funcionalidades mais comuns em redes sociais:
-
-1. **Cadastro**
-
-    Como o projeto está no início, o usuário só precisa informar: o e-mail, nome a sua senha para realizar o cadastro. A senha tem uma regra: ela deve conter, no mínimo, 6 carácteres.
-
-*/
