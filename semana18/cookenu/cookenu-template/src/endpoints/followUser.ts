4ff2c3ae9b9c4f8ebb88ddfe0f9f72f0dd979edc
Ana-Monteiro <anapatricia.c.monteiro@gmail.com>
2021-02-16 00:59:52 -0300

    Começo 2 de followUser

--- semana18/cookenu/cookenu-template/src/endpoints/followUser.ts
+++ semana18/cookenu/cookenu-template/src/endpoints/followUser.ts
@@ -1,2 +1,7 @@
 import { Request, Response } from "express";
 
+export const followUser = async (req: Request, res: Response) => {
+    const token = req.headers.authorization as string
+
+    const userToFollowId = req.body.userToFollowId
+}
